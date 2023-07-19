63a0127e63d81b564f4578a6780c3945b71a00f4
Pattyleinchen <139884575+Pattyleinchen@users.noreply.github.com>
2023-07-19 07:31:23 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,81 +1,48 @@
 // Create web server
-const express = require("express");
-const app = express();
-const port = 3000;
-// Get data from server
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-// Set template engine
-app.set("view engine", "pug");
-app.set("views", "./views");
-// Set static file
-app.use(express.static("public"));
-// Set mongoose
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/express-demo", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-// Create schema
-const commentSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  content: String,
-});
-// Create model
-const Comment = mongoose.model("Comment", commentSchema);
-// Create route
-app.get("/", (req, res) => {
-  res.render("home");
-});
-app.get("/comment", (req, res) => {
-  Comment.find().then((comments) => {
-    res.render("comment/index", {
-      comments: comments,
-    });
-  });
-});
-app.get("/comment/create", (req, res) => {
-  res.render("comment/create");
-});
-app.post("/comment/create", (req, res) => {
-  const comment = new Comment({
-    name: req.body.name,
-    email: req.body.email,
-    content: req.body.content,
-  });
-  comment.save().then(() => {
-    res.redirect("/comment");
-  });
-});
-app.get("/comment/:id", (req, res) => {
-  Comment.findById(req.params.id).then((comment) => {
-    res.render("comment/view", {
-      comment: comment,
-    });
-  });
-});
-app.get("/comment/:id/delete", (req, res) => {
-  Comment.findByIdAndRemove(req.params.id).then(() => {
-    res.redirect("/comment");
-  });
-});
-app.get("/comment/:id/update", (req, res) => {
-  Comment.findById(req.params.id).then((comment) => {
-    res.render("comment/update", {
-      comment: comment,
-    });
-  });
-});
-app.post("/comment/:id/update", (req, res) => {
-  Comment.findByIdAndUpdate(req.params.id, {
-    name: req.body.name,
-    email: req.body.email,
-    content: req.body.content,
-  }).then(() => {
-    res.redirect("/comment");
-  });
-});
-// Listen port
 
+var express = require('express');
+var router = express.Router();
+
+// Database
+var db = require('../models');
+
+// Routes
+router.get('/', function(req, res) {
+    db.Comment.findAll({
+        include: [db.Post]
+    }).then(function(comments) {
+        res.json(comments);
+    });
+});
+
+router.get('/:id', function(req, res) {
+    db.Comment.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [db.Post]
+    }).then(function(comment) {
+        res.json(comment);
+    });
+});
+
+router.post('/', function(req, res) {
+    db.Comment.create({
+        text: req.body.text,
+        PostId: req.body.PostId
+    }).then(function(comment) {
+        res.json(comment);
+    });
+});
+
+router.delete('/:id', function(req, res) {
+    db.Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(function(comment) {
+        res.json(comment);
+    });
+});
+
+module.exports = router;
